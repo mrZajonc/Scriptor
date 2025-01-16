54bcceca8d53458c0e5a5c4fc3a39204e5e062a5
mrZajonc <ptrckjarosz@gmail.com>
2025-01-16 23:33:52 +0100

    1.0.7

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,10 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Przykładowe dane projektów
     const projects = [
-        { id: "p1", title: "Projekt 1", description: "Opis Projektu 1" },
-        { id: "p2", title: "Projekt 2", description: "Opis Projektu 2" },
-        { id: "p3", title: "Projekt 3", description: "Opis Projektu 3" },
-        { id: "p4", title: "Projekt 4", description: "Opis Projektu 4" }
+        { id: "p1", title: "Projekt 1", description: "Opis Projektu 1", animacio: "100" },
+        { id: "p2", title: "Projekt 2", description: "Opis Projektu 2", animacio: "200" },
+        { id: "p3", title: "Projekt 3", description: "Opis Projektu 3", animacio: "300" },
+        { id: "p4", title: "Projekt 4", description: "Opis Projektu 4", animacio: "400" }
     ];
 
     // Generowanie kafelków
@@ -16,10 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Dodanie animacji AOS
         projectDiv.setAttribute("data-aos", "fade-up");
-        projectDiv.setAttribute("data-aos-delay", `${index * 100}`);
+        projectDiv.setAttribute("data-aos-delay", `${index * project.animacio}`);
 
         projectDiv.innerHTML = `
-            <img src="grafika_projekt/${project.id}.jpg" alt="${project.title}">
+            <img src="../projekt_grafik/${project.id}.jpg" alt="${project.title}">
             <h3>${project.title}</h3>
             <p>${project.description}</p>
             <button>Więcej</button>
