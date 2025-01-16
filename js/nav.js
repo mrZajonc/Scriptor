8c5479355af3723bf483b60f4c66aa230058facc
mrZajonc <133904532+mrZajonc@users.noreply.github.com>
2025-01-16 13:44:00 +0100

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('.burger');
   const navLinks = document.querySelector('.nav-links');
+  const navItems = document.querySelectorAll('.nav-links li a');
 
-  burger.addEventListener('click', () => {
+  if (burger && navLinks) {
+    burger.addEventListener('click', () => {
       navLinks.classList.toggle('active');
       burger.classList.toggle('toggle');
-  });
+    });
+
+    // Zamknięcie nawigacji po kliknięciu na link
+    navItems.forEach(item => {
+      item.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        burger.classList.remove('toggle');
+      });
+    });
+  } else {
+    console.error('Burger or nav-links not found');
+  }
 });
