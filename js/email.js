972a6af9d21c84bdd5948dc500d4811a4b1ffbb3
mrZajonc <ptrckjarosz@gmail.com>
2025-01-17 22:31:40 +0100

    1.0.8

--- js/email.js
+++ js/email.js
@@ -18,10 +18,11 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
     emailjs.send("service_ve9phax", "template_u0udsbi", data)
         .then((response) => {
             alert("Wiadomość została wysłana!");
-            this.reset(); // Clear the form
+            document.getElementById('contact-form').reset(); // Clear the form
         })
         .catch((error) => {
             console.error('EmailJS error:', error);
             alert("Nie udało się wysłać wiadomości. Spróbuj ponownie.");
+            document.getElementById('contact-form').reset();
         });
 });
